56d1d358ab7f766d4f854515768a0d8eb2efafe6
rachmatdwis <rchmtdwis@gmail.com>
2024-06-26 09:56:20 +0700

    pushMVc

---
+++ RapidProject.VehicleRentMvc/DAL/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace RapidProject.VehicleRentMvc.DAL.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int id);
+        Task Add(T entity);
+        Task<T> Update(T entity);
+        Task Delete(int id);
+    }
+}
---
+++ RapidProject.VehicleRentMvc/DAL/Repositories/IVehicleRepository.cs
@@ -0,0 +1,10 @@
+﻿using RapidProject.VehicleRentMvc.Models;
+
+namespace RapidProject.VehicleRentMvc.DAL.Repositories
+{
+    public interface IVehicleRepository : IRepository<Vehicle>
+    {
+        Task<IEnumerable<Vehicle>> GetVehiclesByName(string name);
+        Task<IEnumerable<Vehicle>> GetAllVehiclesByType(string typeName);
+    }
+}
